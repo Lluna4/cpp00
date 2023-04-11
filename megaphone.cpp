777cd8a22ede90766ac4d89fb955e1997c4c1739
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-04-11 22:41:15 +0000

    done

--- megaphone.cpp
+++ megaphone.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
 
+int main(int argc, char **argv)
+{
+  int index = 0;
+  if (argc == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  else
+  {
+    argv++;
+    while (*argv)
+    {
+      index = 0;
+      std::string str(*argv);
+      while (str[index])
+      {
+        str[index] = std::toupper(str[index]);
+        index++;
+      }
+      std::cout << str;
+      argv++;
+    }
+    std::cout << std::endl;
+  }
+}
